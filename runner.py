4d2992b0393c2f704697defc03a903c2a7b66f8b
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2025-05-29 15:39:37 +0100

    bugfix

--- runner.py
+++ runner.py
@@ -226,7 +226,7 @@ def write_settings(
     settings_str = settings_f_str.format(
         knot_points=knot_points,
         time_linsys=int(time_linsys),
-        const_update_freq=bool(const_update_freq),
+        const_update_freq=int(const_update_freq),
         adaptive_max_iters=(
             "" if adaptive_max_iters else f"#define PCG_MAX_ITER {max_iters}"
         )
