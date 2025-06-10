4ca4a40c1908ab14399637be7b7f404514c4f3de
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2025-06-10 22:30:32 +0100

    fine grained preconditioning analysis

--- experiments/simple_precond.py
+++ experiments/simple_precond.py
@@ -3,11 +3,12 @@
 from runner import *
 
 if __name__ == "__main__":
-    knot_points = [2,  8, 16]
+    knot_points = [2, 4, 8, 16]
     with expr("with_precond"):
         run_expr(
             knot_points=knot_points,
-            time_linsys=False,
+            time_linsys=True,
+            fine_grained_timing=True,
             adaptive_max_iters=False,
             const_update_freq=True
         )
@@ -15,7 +16,8 @@ if __name__ == "__main__":
     with expr("without_precond"):
         run_expr(
             knot_points=knot_points,
-            time_linsys=False,
+            time_linsys=True,
+            fine_grained_timing=True,
             adaptive_max_iters=False,
             const_update_freq=True,
             enable_preconditioning=False
