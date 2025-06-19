a37474c8699ab97c924d8c691bbf2a9a0ad091f8
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2025-06-19 19:28:20 +0100

    [experiment] for reuse non adaptive sqp time

---
+++ experiments/impact_result_reuse_non_adaptive.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from runner import *
+
+if __name__ == "__main__":
+    init_runner()
+
+    knot_points = [2, 4, 8, 16, 32, 64, 128, 256, 512]
+
+    run_expr(
+        knot_points,
+        Settings(
+            timing_mode=TimingMode.LINSYS,
+            pcg_max_iters=800,
+            sqp_sim_period=2000,
+        ),
+        name_prefix="baseline"
+    )
+
+    run_expr(
+        knot_points,
+        Settings(
+            timing_mode=TimingMode.LINSYS,
+            pcg_max_iters=800,
+            sqp_sim_period=2000,
+            pcg_result_reuse = PCGResultReuse.NO_RESULT_REUSE,
+        ),
+        name_prefix="no_reuse"
+    )
+
+    run_expr(
+        knot_points,
+        Settings(
+            timing_mode=TimingMode.LINSYS,
+            pcg_max_iters=800,
+            sqp_sim_period=2000,
+            pcg_result_reuse = PCGResultReuse.MEASURE_CLOSENESS_INITIAL_GUESS
+        ),
+        name_prefix="measure_closeness_initial_guess"
+    )
