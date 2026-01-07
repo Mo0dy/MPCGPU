1b522c231f4116789c43adc3f11946c2b88e6de4
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2026-01-07 17:02:44 +0100

    line_search experiment setup

--- experiments/runner.py
+++ experiments/runner.py
@@ -27,6 +27,16 @@ class TimingMode(Enum):
         # without TimingMode.
         return self.name.lower()
 
+
+# NOTE: should match the defines in settings.cuh
+class LineSearchMode(Enum):
+    EXP_GRID = 1
+    ACADOS_BACKTRACKING = 2
+    FULLSTEP_01 = 3
+
+    def __str__(self):
+        return self.name.lower()
+
 ADAPTIVE = "adaptive"
 Adaptive: TypeAlias = Literal["adaptive"]
 PCGMaxIters: TypeAlias = Union[int, Adaptive]
@@ -41,6 +51,7 @@ class Settings:
     pcg_max_iters: PCGMaxIters
     sqp_sim_period: SimPeriod = 2000
     enable_preconditioning: bool = True
+    line_search_version: LineSearchMode = LineSearchMode.FULLSTEP_01
 
     @classmethod
     def default(cls):
@@ -101,6 +112,43 @@ results_dir = project_root / "results"
 results_dir.mkdir(parents=True, exist_ok=True)
 
 settings_f_str = """#pragma once
+
+// ===============================================
+// Line search settings
+// ===============================================
+
+//MPCGPU default
+//for these settings use 8 sampling points (parallel streams)
+
+#define LINE_SEARCH_EXP_GRID 1
+//Acados default backtracking selection criteria
+#define LINE_SEARCH_ACADOS_BACKTRACKING 2
+//functions as baseline -> always use QP solution (since alpha = 1 is often selected as best)
+#define LINE_SEARCH_FULLSTEP_01 3
+
+//for these settings use 50 sampling points (parallel streams)
+#define LINE_SEARCH_LINEAR_MINIMUM_50P 4
+#define LINE_SEARCH_WEIGHTED_BELL_A_09_S_1_50P 5
+#define LINE_SEARCH_WEIGHTED_BELL_A_08_S_1_50P 6
+#define LINE_SEARCH_WEIGHTED_BELL_A_0999_S_1_50P 7
+//for these settings use 100 sampling points (parallel streams)
+#define LINE_SEARCH_LINEAR_MINIMUM_100P 8
+#define LINE_SEARCH_WEIGHTED_BELL_A_09_S_1_100P 9
+#define LINE_SEARCH_WEIGHTED_BELL_A_08_S_1_100P 10
+#define LINE_SEARCH_WEIGHTED_BELL_A_0999_S_1_100P 11
+
+#define LINE_SEARCH_VERSION {{line_search_version}}
+
+//parallel streams can be set by using LINE_SEARCH_VERSION
+#if LINE_SEARCH_VERSION < 4
+#define NUM_ALPHAS 8
+#elif LINE_SEARCH_VERSION < 8
+#define NUM_ALPHAS 50
+//can be replaced
+#elif LINE_SEARCH_VERSION < 12
+#define NUM_ALPHAS 100
+#endif
+
 
 #ifndef KNOT_POINTS
 #define KNOT_POINTS {knot_points}
@@ -323,7 +371,8 @@ def write_settings(
         const_update_freq=const_update_freq,
         adaptive_max_iters=adaptive_max_iters_str,
         simulation_period=simulation_period,
-        enable_preconditioning=enable_preconditioning
+        enable_preconditioning=enable_preconditioning,
+        line_search_version=settings.line_search_version.value
     )
 
     with open(settings_file, 'w') as f:
