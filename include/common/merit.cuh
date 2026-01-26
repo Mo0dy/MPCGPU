f403324e8e514fe495a0b0561fa504a01d7575ca
Felix Mühlenberend <felix.muehlenberend@mailbox.org>
2026-01-26 14:48:44 +0100

    reset mpccode

--- include/common/merit.cuh
+++ include/common/merit.cuh
@@ -1,29 +1,38 @@
 #pragma once
 
+#include <cstdint>
+#include <cooperative_groups.h>
 #include "dynamics/rbd_plant.cuh"
 #include "integrator.cuh"
-#include "settings.cuh"
-#include <cooperative_groups.h>
-#include <cstdint>
 
-// TODO: this
+//TODO: this
 template <typename T>
-size_t get_merit_smem_size(uint32_t state_size, uint32_t control_size) {
-    return sizeof(T) *
-           (6 + (2 * state_size + control_size) + ((int)1.5 * state_size) +
-            gato_plant::forwardDynamics_TempMemSize_Shared());
+size_t get_merit_smem_size(uint32_t state_size, uint32_t control_size)
+{
+    return sizeof(T) * (6 + (2 * state_size + control_size ) + 
+                        ((int) 1.5 * state_size) + gato_plant::forwardDynamics_TempMemSize_Shared());
 }
 
 // cost compute for line search
 template <typename T>
-__global__ void ls_gato_compute_merit(
-    uint32_t state_size, uint32_t control_size, uint32_t knot_points, T *d_xs,
-    T *d_xu, T *d_eePos_traj, T mu, T dt, void *d_dynMem_const, T *d_dz,
-    uint32_t alpha_multiplier, T *d_merits_out, T *d_merit_temp) {
+__global__
+void ls_gato_compute_merit(uint32_t state_size,
+                           uint32_t control_size,
+                           uint32_t knot_points,
+                           T *d_xs,
+                           T *d_xu, 
+                           T *d_eePos_traj, 
+                           T mu, 
+                           T dt, 
+                           void *d_dynMem_const, 
+                           T *d_dz,
+                           uint32_t alpha_multiplier, 
+                           T *d_merits_out, 
+                           T *d_merit_temp)
+{
 
     grid::robotModel<T> *d_robotModel = (grid::robotModel<T> *)d_dynMem_const;
-    const cooperative_groups::thread_block block =
-        cooperative_groups::this_thread_block();
+    const cooperative_groups::thread_block block = cooperative_groups::this_thread_block();
     const uint32_t thread_id = threadIdx.x;
     const uint32_t num_threads = blockDim.x;
     const uint32_t block_id = blockIdx.x;
@@ -35,70 +44,31 @@ __global__ void ls_gato_compute_merit(
 
     T Jk, ck, pointmerit;
 
-
-const uint32_t num_alphas = NUM_ALPHAS;
-//#if NUM_ALPHAS == 8
-//    const uint32_t num_alphas = 8;
-//#elif NUM_ALPHAS == 50
-//    const uint32_t num_alphas = 50;
-//#elif NUM_ALPHAS == 100
-//    const uint32_t num_alphas = 100;
-//#else
-//        assert(false && "Invalid NUM_ALPHAS");
-//#endif
-    const T alpha_min = 1e-18;
-    const T alpha_max = 2;
-
-#if LINE_SEARCH_VERSION == LINE_SEARCH_EXP_GRID || LINE_SEARCH_VERSION == LINE_SEARCH_EXP_GRID_Mod8_No_Bell || LINE_SEARCH_VERSION == LINE_SEARCH_EXP_GRID_Mod8_Bell
-    // @LSK_0: calculate alpha (exp grid)
-
-    T alpha = -1.0 / (1 << (alpha_multiplier%8));
-#elif LINE_SEARCH_VERSION == LINE_SEARCH_EXP_ACADOS_BACKTRACKING
-    // @LSK_0: calculate alpha (acados like)
-    T alpha = -1.0 / (1 << alpha_multiplier);
-#elif LINE_SEARCH_VERSION == LINE_SEARCH_FULLSTEP_01
-        //full_step returns 8x (-1.0, merit(-1.0))
-    T alpha = -1.0;
-
-//every other implementation uses a quadratic spaced sampling of alpha
-#elif LINE_SEARCH_VERSION >=4
-        T to_add = (alpha_max-alpha_min)/((T) num_alphas);
-        T alpha = (-1.0)*(alpha_min + to_add * (T)alpha_multiplier)*(alpha_min + to_add * (T)alpha_multiplier);
-#else
-    #error "LINE_SEARCH_VERSION not defined"
-#endif //LINE_SEARCH_VERSION
-
-
-    T *s_eePos_k_traj = s_xux_k + 2 * state_size + control_size;
+    T alpha = -1.0 / (1 << alpha_multiplier);   // alpha sign
+    T *s_eePos_k_traj = s_xux_k + 2*state_size+control_size;
     T *s_temp = s_eePos_k_traj + 6;
 
-    // @LSK_1: sum integration error and tracking cost over (block of) knots
-    for (unsigned knot = block_id; knot < knot_points; knot += num_blocks) {
 
-        for (int i = thread_id;
-             i < state_size + (knot < knot_points - 1) * (states_s_controls);
-             i += num_threads) {
-            s_xux_k[i] = d_xu[knot * states_s_controls + i] +
-                         alpha * d_dz[knot * states_s_controls + i];
-            if (i < 6) {
-                s_eePos_k_traj[i] = d_eePos_traj[knot * 6 + i];
+    for(unsigned knot = block_id; knot < knot_points; knot += num_blocks){
+
+        for(int i = thread_id; i < state_size+(knot < knot_points-1)*(states_s_controls); i+=num_threads){
+            s_xux_k[i] = d_xu[knot*states_s_controls+i] + alpha * d_dz[knot*states_s_controls+i];  
+            if (i < 6){
+                s_eePos_k_traj[i] = d_eePos_traj[knot*6+i];                            
             }
         }
         block.sync();
-
-        Jk = gato_plant::trackingcost<T>(state_size, control_size, knot_points,
-                                         s_xux_k, s_eePos_k_traj, s_temp,
-                                         d_robotModel);
-
+        
+        Jk = gato_plant::trackingcost<T>(state_size, control_size, knot_points, s_xux_k, s_eePos_k_traj, s_temp, d_robotModel);
+        
         block.sync();
-        if (knot < knot_points - 1) {
-            ck = integratorError<T>(state_size, s_xux_k,
-                                    &s_xux_k[states_s_controls], s_temp,
-                                    d_robotModel, dt, block);
-        } else {
+        if(knot < knot_points-1){
+            ck = integratorError<T>(state_size, s_xux_k, &s_xux_k[states_s_controls], s_temp, d_robotModel, dt, block);
+        }
+        else{
             // diff xs vs xs_traj
-            for (int i = threadIdx.x; i < state_size; i++) {
-                s_temp[i] = abs((d_xu[i] + alpha * d_dz[i]) - d_xs[i]);
+            for(int i = threadIdx.x; i < state_size; i++){
+                s_temp[i] = abs((d_xu[i] + alpha *d_dz[i]) - d_xs[i]);
             }
             block.sync();
             glass::reduce<T>(state_size, s_temp);
@@ -107,23 +77,18 @@ const uint32_t num_alphas = NUM_ALPHAS;
         }
         block.sync();
 
-        if (thread_id == 0) {
-            pointmerit = Jk + mu * ck;
-            d_merit_temp[alpha_multiplier * knot_points + knot] = pointmerit;
-            // printf("alpha: %f knot: %d reporting merit: %f\n", alpha, knot,
-            // pointmerit);
+        if(thread_id == 0){
+            pointmerit = Jk + mu*ck;
+            d_merit_temp[alpha_multiplier*knot_points+knot] = pointmerit;
+            // printf("alpha: %f knot: %d reporting merit: %f\n", alpha, knot, pointmerit);
         }
     }
-
-    // @LSK_2: reduction
     cooperative_groups::this_grid().sync();
-    if (block_id == 0) {
-        glass::reduce<T>(knot_points,
-                         &d_merit_temp[alpha_multiplier * knot_points]);
-
-        if (thread_id == 0) {
-            d_merits_out[alpha_multiplier] =
-                d_merit_temp[alpha_multiplier * knot_points];
+    if(block_id == 0){
+        glass::reduce<T>(knot_points, &d_merit_temp[alpha_multiplier*knot_points]);
+    
+        if(thread_id == 0){
+            d_merits_out[alpha_multiplier] = d_merit_temp[alpha_multiplier*knot_points];
         }
     }
 }
@@ -132,13 +97,11 @@ const uint32_t num_alphas = NUM_ALPHAS;
 // shared mem size get_merit_smem_size()
 // cost compute for non line search
 template <typename T, unsigned INTEGRATOR_TYPE = 0, bool ANGLE_WRAP = false>
-__global__ void compute_merit(uint32_t state_size, uint32_t control_size,
-                              uint32_t knot_points, T *d_xu, T *d_eePos_traj,
-                              T mu, T dt, void *d_dynMem_const,
-                              T *d_merit_out) {
+__global__
+void compute_merit(uint32_t state_size, uint32_t control_size, uint32_t knot_points, T *d_xu, T *d_eePos_traj, T mu, T dt, void *d_dynMem_const, T *d_merit_out)
+{
     grid::robotModel<T> *d_robotModel = (grid::robotModel<T> *)d_dynMem_const;
-    const cooperative_groups::thread_block block =
-        cooperative_groups::this_thread_block();
+    const cooperative_groups::thread_block block = cooperative_groups::this_thread_block();
     const uint32_t thread_id = threadIdx.x;
     const uint32_t num_threads = blockDim.x;
     const uint32_t block_id = blockIdx.x;
@@ -150,34 +113,30 @@ __global__ void compute_merit(uint32_t state_size, uint32_t control_size,
     T *s_eePos_k_traj = s_xux_k + 2 * state_size + control_size;
     T *s_temp = s_eePos_k_traj + 6;
 
-    for (unsigned knot = block_id; knot < knot_points; knot += gridDim.x) {
+    for(unsigned knot = block_id; knot < knot_points; knot += gridDim.x){
 
-        for (int i = thread_id;
-             i < state_size + (knot < knot_points - 1) * (states_s_controls);
-             i += num_threads) {
-            s_xux_k[i] = d_xu[knot * states_s_controls + i];
-            if (i < 6) {
-                s_eePos_k_traj[i] = d_eePos_traj[knot * 6 + i];
+        for(int i = thread_id; i < state_size+(knot < knot_points-1)*(states_s_controls); i+=num_threads){
+            s_xux_k[i] = d_xu[knot*states_s_controls+i];  
+            if (i < 6){
+                s_eePos_k_traj[i] = d_eePos_traj[knot*6+i];                            
             }
         }
 
         block.sync();
-        Jk = gato_plant::trackingcost<T>(state_size, control_size, knot_points,
-                                         s_xux_k, s_eePos_k_traj, s_temp,
-                                         d_robotModel);
+        Jk = gato_plant::trackingcost<T>(state_size, control_size, knot_points, s_xux_k, s_eePos_k_traj, s_temp, d_robotModel);
+
 
         block.sync();
-        if (knot < knot_points - 1) {
-            ck = integratorError<T>(state_size, s_xux_k,
-                                    &s_xux_k[states_s_controls], s_temp,
-                                    d_robotModel, dt, block);
-        } else {
+        if(knot < knot_points-1){
+            ck = integratorError<T>(state_size, s_xux_k, &s_xux_k[states_s_controls], s_temp, d_robotModel, dt, block);
+        }
+        else{
             ck = 0;
         }
         block.sync();
 
-        if (thread_id == 0) {
-            pointmerit = Jk + mu * ck;
+        if(thread_id == 0){
+            pointmerit = Jk + mu*ck;
             atomicAdd(d_merit_out, pointmerit);
         }
     }
--- include/common/settings.cuh
+++ include/common/settings.cuh
@@ -1,57 +1,17 @@
 #pragma once
 
 // ===============================================
-// Line search settings
+// My Settings
 // ===============================================
 
-//MPCGPU default
-//for these settings use 8 sampling points (parallel streams)
-#define LINE_SEARCH_EXP_GRID 1
+#define ENABLE_PRECONDITIONING 1
 
-//Acados default backtracking selection criteria
-#define LINE_SEARCH_EXP_ACADOS_BACKTRACKING 2
-//functions as baseline -> always use QP solution (since alpha = 1 is often selected as best)
-#define LINE_SEARCH_FULLSTEP_01 3
-
-//for these settings use 50 sampling points (parallel streams)
-#define LINE_SEARCH_QUADR_MINIMUM_50P 4
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_09_S_1_50P 5
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_08_S_1_50P 6
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_0999_S_1_50P 7
-//for these settings use 100 sampling points (parallel streams)
-#define LINE_SEARCH_QUADR_MINIMUM_100P 8
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_09_S_1_100P 9
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_08_S_1_100P 10
-#define LINE_SEARCH_QUADR_WEIGHTED_BELL_A_0999_S_1_100P 11
-
-#define LINE_SEARCH_EXP_GRID_Mod8_No_Bell 12
-#define LINE_SEARCH_QUADRATIC_GRID_No_Bell 13
-#define LINE_SEARCH_QUADRATIC_GRID_Bell 14
-#define LINE_SEARCH_EXP_GRID_Mod8_Bell 15
-
-#define LINE_SEARCH_VERSION 15
-
-#define USE_TOL_END_CRITERION 1
-
-//parallel streams can be set by using LINE_SEARCH_VERSION
-#if LINE_SEARCH_VERSION < 4
-#define NUM_ALPHAS 8
-#elif LINE_SEARCH_VERSION < 8
-//#define NUM_ALPHAS 50
-#define NUM_ALPHAS 8
-//can be replaced
-#elif LINE_SEARCH_VERSION < 12
-//#define NUM_ALPHAS 100
-#define NUM_ALPHAS 8
-#else
-//set alpha here if wanting to run an experiment with same alphas for the last methods
-//#define NUM_ALPHAS 32
-#define NUM_ALPHAS 8
-#endif
-
+// ===============================================
+// Their Settings
+// ===============================================
 
 #ifndef KNOT_POINTS
-#define KNOT_POINTS 128
+#define KNOT_POINTS 2
 #endif
 
 // default value is for iiwa arm
@@ -145,15 +105,15 @@ typedef float linsys_t;
  *                           Linsys Settings                               *
  *******************************************************************************/
 
-#define ENABLE_PRECONDITIONING 1
 
 /* time_linsys = 1 to record linear system solve times.
 time_linsys = 0 to record number of sqp iterations.
 In both cases, the tracking error will also be recorded. */
 
-#define TIME_LINSYS 1
-#define FINE_GRAINED_TIMING 1
+#define TIME_LINSYS 0
+#define FINE_GRAINED_TIMING 0
 
+// if fine grained timing is enabled require that time linsys is also enabled
 #if FINE_GRAINED_TIMING && !TIME_LINSYS
 #error "Fine grained timing requires time linsys to be enabled"
 #endif
@@ -170,7 +130,7 @@ LINSYS_SOLVE = 0 uses qdldl as the underlying linear system solver */
 #define LINSYS_SOLVE 1
 #endif
 
-
+#define PCG_MAX_ITER 10000
 
 // Values found using experiments
 #ifndef PCG_MAX_ITER
@@ -202,16 +162,16 @@ LINSYS_SOLVE = 0 uses qdldl as the underlying linear system solver */
 
 
 #if TIME_LINSYS == 1
-    #define SQP_MAX_ITER    60
+    #define SQP_MAX_ITER    20
     typedef double toplevel_return_type;
 #else
-    #define SQP_MAX_ITER    60
+    #define SQP_MAX_ITER    40
     typedef uint32_t toplevel_return_type;
 #endif
 
 
 #ifndef SQP_MAX_TIME_US
-#define SQP_MAX_TIME_US 100000
+#define SQP_MAX_TIME_US 2000
 #endif
 
 #ifndef SCHUR_THREADS
