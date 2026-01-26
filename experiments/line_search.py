2abce3be8fa6f4f3e825cb16b76bed233bd5d2e5
Felix <felix@fedora.fritz.box>
2026-01-26 18:53:25 +0100

    line_search expr now with const sqp time

--- experiments/line_search.py
+++ experiments/line_search.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     #knot_points = [2, 4, 8, 16, 32, 64, 128, 256, 512]
     # knot_points = [2, 4, 8]
     #Felix wanted 32 for reasonable results:
-    knot_points = [32]
+    knot_points = [16]
     # Baseline. The settings the paper authors used for the experiments
 
     for ls_version in LineSearchMode:
@@ -22,12 +22,13 @@ if __name__ == "__main__":
             knot_points,
             Settings(
                 # FINE_GRAINED is also interesting.
-                timing_mode=TimingMode.MINIMAL,
+                timing_mode=TimingMode.FINE_GRAINED,
                 # ADAPTIVE == early termination in pcg
                 # same as used in the MPCGPU paper experiments
                 pcg_max_iters=ADAPTIVE,
                 # leave as is
                 sqp_sim_period=2000,
+                sqp_max_time_us=2000,
                 line_search_version=ls_version
             ),
             name_prefix=str(ls_version),
